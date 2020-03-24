ba312f5aa0a5110b1d477adaf81f22d715d428bb
Андрей <andrey@whatasoft.net>
2020-03-24 23:54:24 +0500

    Initial commit

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+(function() {
+    const resultField = document.querySelector('#result');
+    const clearBtn = document.querySelector('#clear');
+    const digitButtons = document.querySelectorAll('.digit-button');
+    const operatorButtons = document.querySelectorAll('.operator-button');
+
+    /* Допишите ваш JS код здесь */
+})()
