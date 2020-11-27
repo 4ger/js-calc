e59acc0fc734c3ec12e013abe5393d6e4d3c649a
4ger <djvu.master@gmail.com>
2020-11-27 22:13:40 +0500

    0.1 initial commit.

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//alert( 'THIS');
+//alert(typeof myval);
+
+(function() {
+    const resultField = document.querySelector('#result');
+    const clearBtn = document.querySelector('#clear');
+    const digitButtons = document.querySelectorAll('.digit-button');
+    const operatorButtons = document.querySelectorAll('.operator-button');
+
+    /* Допишите ваш JS код здесь */
+})()
